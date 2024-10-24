18d17cca096bb4e89ff555949bb77b29f9598d3b
ajuarezse <ajuarez.se@gmail.com>
2024-10-24 09:25:22 -0700

    configure auth and protected route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,33 @@
+import { useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+
 import Ducks from "./Ducks";
 import Login from "./Login";
 import MyProfile from "./MyProfile";
 import Register from "./Register";
+import ProtectedRoute from "./ProtectedRoute";
 import "./styles/App.css";
-import { useState } from "react";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <Routes>
-      <Route path="/ducks" element={<Ducks />} />
-      <Route path="/my-profile" element={<MyProfile />} />
+      <Route
+        path="/ducks"
+        element={
+          <ProtectedRoute isLoggedIn={isLoggedIn}>
+            <Ducks />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/my-profile"
+        element={
+          <ProtectedRoute isLoggedIn={isLoggedIn}>
+            <MyProfile />
+          </ProtectedRoute>
+        }
+      />
       <Route
         path="/login"
         element={
@@ -31,7 +47,11 @@ function App() {
       <Route
         path="*"
         element={
-          isLoggedIn ? <Navigate to="/ducks" /> : <Navigate to="/login" />
+          isLoggedIn ? (
+            <Navigate to="/ducks" replace />
+          ) : (
+            <Navigate to="/login" replace />
+          )
         }
       />
     </Routes>
